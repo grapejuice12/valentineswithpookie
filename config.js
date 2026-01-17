b2896fd443f8595fc404146977a1cd512d909638
grapejuice12 <148267377+grapejuice12@users.noreply.github.com>
2026-01-17 09:11:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/E-UJTGXof6Q?si=YJbjjUmTDmormOwM", // Music streaming URL
+        musicUrl: "https://soundcloud.com/brunomars/just-the-way-you-are-2?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
